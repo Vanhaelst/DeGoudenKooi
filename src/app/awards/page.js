16ba7241e815e1f72c4ee0370c9ff89427e76267
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2024-12-17 13:51:31 +0100

    add pages

---
+++ src/app/awards/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Footer, MegaMenu, TopBar } from "@/components/organisms";
+import { fetchData } from "@/utils/fetchData";
+import { PageQuery } from "@/queries/sections/page";
+import { renderComponents } from "@/utils/renderComponents";
+
+async function getPage() {
+  return fetchData(PageQuery({ page: "awardspageEntries" }));
+}
+
+export default async function Home() {
+  const { page } = await getPage();
+
+  const sections = page[0]?.sections;
+
+  return (
+    <div>
+      <TopBar />
+      <MegaMenu />
+
+      {sections?.map((section) => renderComponents(section))}
+
+      <Footer />
+    </div>
+  );
+}
