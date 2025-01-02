72a50a8de472c68ab4c5784e300dd648ebb5d1b9
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-01-02 11:00:16 +0100

    update bookeo

--- src/components/organisms/Bookeo/bookeo.js
+++ src/components/organisms/Bookeo/bookeo.js
@@ -4,18 +4,13 @@ import Script from "next/script";
 export const Bookeo = () => {
   return (
     <>
-      <Script
+      <p>-- START BOOKEO --</p>
+
+      <script
         type="text/javascript"
         src="https://bookeo.com/widget.js?a=3250KXLLEU151F84FE360&type=325043P7UH151F85563A6"
-      ></Script>
-      <section id="book">
-        <Container>
-          <iframe
-            className="w-full h-[500px] py-24"
-            src="https://www-2552n.bookeo.com/bookeo/b_viewGenericGiftVouchers.html?bsid=pehkxw9prtawfj94&backNav=true&ncs=k93pnuaj7e&ncs2=hxnt6xrtla"
-          />
-        </Container>
-      </section>
+      />
+      <p>-- END BOOKEO --</p>
     </>
   );
 };
