d2bf8c8b1c14890c8bb7e1026bd41039df8d0aa3
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-01-02 17:11:39 +0100

    update bookeo

--- src/components/organisms/Bookeo/bookeo.js
+++ src/components/organisms/Bookeo/bookeo.js
@@ -9,6 +9,7 @@ export const Bookeo = () => {
         type="text/javascript"
         src="https://bookeo.com/widget.js?a=3250KXLLEU151F84FE360&category=325073FEXL162392927A1"
       ></Script>
+      <div className="wsb-canvas-page-container" />
       <p>-- END BOOKEO --</p>
     </>
   );
