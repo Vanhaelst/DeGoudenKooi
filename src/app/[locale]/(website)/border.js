c2d220345bd3b5cc0a2fc427805a0697616af47b
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-04-07 08:36:37 +0200

    less space for borders

--- src/app/[locale]/(website)/border.js
+++ src/app/[locale]/(website)/border.js
@@ -25,10 +25,10 @@ export const Borders = () => {
         width={92}
         height={92}
       />
-      <div className="border md:border-2 absolute border-primary-500 w-[calc(100%_-_1rem_-_65px)] md:w-[calc(100%_-_1rem_-_92px)] border-top left-2 top-2" />
-      <div className="border md:border-2 absolute border-primary-500 w-[calc(100%_-_1rem_-_65px)] md:w-[calc(100%_-_1rem_-_92px)] border-bottom right-2 bottom-2" />
-      <div className="border md:border-2 absolute border-primary-500 h-[calc(100%_-_1rem_-_65px)] md:h-[calc(100%_-_1rem_-_92px)] border-left left-2 top-2" />
-      <div className="border md:border-2 absolute border-primary-500 h-[calc(100%_-_1rem_-_65px)] md:h-[calc(100%_-_1rem_-_92px)] border-right right-2 bottom-2" />
+      <div className="border md:border-2 absolute border-primary-500 w-[calc(100%_-_1rem_-_65px)] md:w-[calc(100%_-_1rem_-_92px)] border-top left-1 top-1" />
+      <div className="border md:border-2 absolute border-primary-500 w-[calc(100%_-_1rem_-_65px)] md:w-[calc(100%_-_1rem_-_92px)] border-bottom right-1 bottom-1" />
+      <div className="border md:border-2 absolute border-primary-500 h-[calc(100%_-_1rem_-_65px)] md:h-[calc(100%_-_1rem_-_92px)] border-left left-1 top-1" />
+      <div className="border md:border-2 absolute border-primary-500 h-[calc(100%_-_1rem_-_65px)] md:h-[calc(100%_-_1rem_-_92px)] border-right right-1 bottom-1" />
       <Image
         className="absolute bottom-2 left-2 w-14 md:w-24"
         src="/border-bottom-left.png"
