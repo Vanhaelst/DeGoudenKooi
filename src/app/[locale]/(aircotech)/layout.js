967e834e3f4bd5df3e42b2d74ea5e7dca13768c6
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-04-09 09:55:53 +0200

    update cookiebot

--- src/app/[locale]/(aircotech)/layout.js
+++ src/app/[locale]/(aircotech)/layout.js
@@ -25,16 +25,39 @@ export default function RootLayout({ children, params }) {
       >
         {children}
 
-        <Script
-          src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"
-          strategy="beforeInteractive"
-        />
-        <Script
-          id="usercentrics-cmp"
-          src="https://web.cmp.usercentrics.eu/ui/loader.js"
-          data-settings-id="Q_xuNFhDQfahYX"
-          strategy="beforeInteractive"
-        />
+        {process.env.NODE_ENV !== "production" ? (
+          <Script
+            id="usercentrics-cmp"
+            src="https://web.cmp.usercentrics.eu/ui/loader.js"
+            data-draft="true"
+            strategy="beforeInteractive"
+            data-settings-id="Q_xuNFhDQfahYX"
+            async
+          />
+        ) : process.env.NEXT_PUBLIC_COOKIE_TYPE !== "auto" ? (
+          // auto block
+          <>
+            <Script
+              src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"
+              strategy="beforeInteractive"
+            />
+            <Script
+              id="usercentrics-cmp"
+              src="https://web.cmp.usercentrics.eu/ui/loader.js"
+              data-settings-id="Q_xuNFhDQfahYX"
+              strategy="beforeInteractive"
+            />
+          </>
+        ) : (
+          // manual block
+          <Script
+            id="usercentrics-cmp"
+            src="https://web.cmp.usercentrics.eu/ui/loader.js"
+            strategy="beforeInteractive"
+            data-settings-id="Q_xuNFhDQfahYX"
+            async
+          />
+        )}
       </body>
     </html>
   );
