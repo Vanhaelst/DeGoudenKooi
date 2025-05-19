10b126f8737b81e9b94978d90463096128a9bdeb
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-05-19 17:35:13 +0200

    Update paddings

--- src/components/organisms/features/features.js
+++ src/components/organisms/features/features.js
@@ -13,7 +13,7 @@ export const Features = ({
   const bgColor = getBackgroundColor(backgroundColor);
   return (
     <section className={`${bgColor} py-12 sm:py-16`}>
-      <Container classnames="mb-24">
+      <Container classnames="mb-10">
         <Title title={title} description={description} center={center} />
       </Container>
       <Container classnames="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
