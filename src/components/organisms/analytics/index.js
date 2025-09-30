f213385ebe4fa3d2a0233ac1f90ca8a4d0679777
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2025-09-30 09:47:10 +0200

    update GTM

--- src/components/organisms/analytics/index.js
+++ src/components/organisms/analytics/index.js
@@ -4,12 +4,18 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import React from "react";
 
 export const GoogleAnalytics = () => {
-  const cookieBot = window?.localStorage.getItem("ucData");
-  const analyticsStorage = JSON.parse(cookieBot)?.gcm?.analyticsStorage;
+  if (typeof window !== "undefined") {
+    const cookieBot = window?.localStorage.getItem("ucData");
+    const analyticsStorage = JSON.parse(cookieBot)?.gcm?.analyticsStorage;
 
-  return (
-    <GoogleTagManager
-      gtmId={analyticsStorage === "granted" ? process.env.NEXT_PUBLIC_GTM : ""}
-    />
-  );
+    return (
+      <GoogleTagManager
+        gtmId={
+          analyticsStorage === "granted" ? process.env.NEXT_PUBLIC_GTM : ""
+        }
+      />
+    );
+  }
+
+  return null;
 };
